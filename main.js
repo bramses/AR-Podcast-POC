6c3d20395518ea6f9d5be226d8021074b6095984
Bram Adams <3282661+bramses@users.noreply.github.com>
2020-06-09 16:57:55 -0400

    added logic to manage table

--- main.js
+++ main.js
@@ -36,27 +36,86 @@ function notifyMe() {
     // want to be respectful there is no need to bother them any more.
 }
 
-function main () {
-    setUpSlider()
-}
-
-window.addEventListener("load", main)
-
 
 function calculateActiveTable (showNote, currentTime) {
   return showNote.timeActive[0] < currentTime && showNote.timeActive[1] > currentTime
 }
 
+let timeElapsed = 0
+
+function startTimer (audioLength) {
+  setInterval(() => {
+    if (timeElapsed < audioLength) timeElapsed++
+  },1000)
+}
+
+/*
+TODO: render an empty table
+TODO: try to stagger objects showing up in table
+*/
+function renderTable (data) {
+  document.getElementById("wrapper").innerHTML = ""
+  console.log(data)
+  new gridjs.Grid({
+    columns: ["Name", "Link"],
+    data: data
+  }).render(document.getElementById("wrapper"))
+}
+
+function renderTableData (data) {
+  const newTable = []
+  for (let i = 0; i < data.length; i++) {
+    const row = data[i]
+    // console.log(calculateActiveTable(row, timeElapsed))
+    if (calculateActiveTable(row, timeElapsed)) {
+      newTable.push([row.objName, row.objLink])
+    }
+  }
+
+  return newTable
+}
+
 class ShowNotes {
   
   constructor (objName, objLink, timeActive) {
-
+    this.objName = objName
+    this.objLink = objLink
+    this.timeActive = timeActive
   }
 
 }
 
 class AudioMetadata {
-  constructor (audioName, audio) {
-    
+  constructor (audioName, audio, audioLength) {
+    this.audioName = audioName
+    this.audio = audio
+    this.audioLength = audioLength
   }
 }
+
+
+function main () {
+    setUpSlider()
+    const showNotes = new ShowNotes("elephant", "/assets/models/Elephant.usdz", [0, 10])
+    const audio = new AudioMetadata("Naval On Truth", "/assets/audio/Naval-Ep67.mp3", 124)
+    startTimer(audio.audioLength)
+    
+    const data = [showNotes]
+    setInterval(() => renderTable(renderTableData(data)), 1000)
+
+}
+
+// new gridjs.Grid({
+//   columns: ["Name", "Email", "Phone Number"],
+//   data: [
+//     ["John", "john@example.com", "(353) 01 222 3333"],
+//     ["Mark", "mark@gmail.com", "(01) 22 888 4444"],
+//     ["Eoin", "eoin@gmail.com", "0097 22 654 00033"],
+//     ["Sarah", "sarahcdd@gmail.com", "+322 876 1233"],
+//     ["Afshin", "afshin@mail.com", "(353) 22 87 8356"]
+//   ]
+// }).render(document.getElementById("wrapper"));
+
+window.addEventListener("load", main)
+
+
